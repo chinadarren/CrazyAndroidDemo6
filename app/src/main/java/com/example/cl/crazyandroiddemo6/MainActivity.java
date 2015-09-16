603350f4afad0a6a5147a0322043cfa20cc9f013
chinadarren <chinadarren@outlook.com>
2015-09-16 19:50:45 +0800

    ss

--- app/src/main/java/com/example/cl/crazyandroiddemo6/MainActivity.java
+++ app/src/main/java/com/example/cl/crazyandroiddemo6/MainActivity.java
@@ -1,38 +1,45 @@
 package com.example.cl.crazyandroiddemo6;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.GridLayout;
 
-
-public class MainActivity extends ActionBarActivity {
-
+/**
+ * Java代码采用循环控制添加16个按钮
+ */
+public class MainActivity extends Activity {
+    GridLayout gridLayout;
+    //定义16个按钮的文本
+    String [] chars = new String []{
+            "7","8","9","/",
+            "4","5","6","x",
+            "1","2","3","-",
+            ".","0","=","+"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        setContentView(R.layout.counter_gui);
+        gridLayout = (GridLayout) findViewById(R.id.root);
+        for (int i = 0; i <chars.length;i++){
+            Button bn = new Button(this);
+            bn.setText(chars[i]);
+            //色环之按钮的字号大小
+            bn.setTextSize(40);
+            //指定该组件所在的行
+            GridLayout.Spec rowSpec = GridLayout.spec(i / 4+2);
+            //指定该组件所在的列
+            GridLayout.Spec columnSpec = GridLayout.spec(i % 4);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec,columnSpec);
+            //指定该组件占满父容器
+            params.setGravity(Gravity.FILL);
+            gridLayout.addView(bn,params);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
